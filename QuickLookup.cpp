78f8a0cf8444195a1b8e575ca78463b2011877bf
andydexter <andydexter123@gmail.com>
2024-08-21 23:50:31 +0300

    Add project files.

---
+++ QuickLookup.cpp
@@ -0,0 +1,12 @@
+﻿// QuickLookup.cpp : Defines the entry point for the application.
+//
+
+#include "QuickLookup.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
