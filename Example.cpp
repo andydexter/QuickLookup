60737cfcedd402f3d011b6605f20f686f6c73e7d
andydexter <andydexter123@gmail.com>
2024-09-09 12:12:58 +0200

    moved header files to /src

--- Example.cpp
+++ Example.cpp
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "QuickLookup.h"
+#include "src/QuickLookup.h"
 #include <iostream>
 #include <map>
 #include <algorithm>
